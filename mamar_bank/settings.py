17223dfcdc4cfe1fd6b35b4fb8a8c70b1198a70f
Abdullah-Al-Sadid <abdullahalsadid1914@gmail.com>
2024-06-19 22:15:18 +0600

    CSRF

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://mamar-bank-lbln.onrender.com']
+
 
 
 # Application definition
